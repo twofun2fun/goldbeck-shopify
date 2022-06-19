95ce408e97aeb54fce05bfdf4ad38d6bb700ad32
twofun2fun <tufan.calisir@outlook.de>
2022-06-19 23:27:11 +0200

    feat: basicfeatures added as customer wished

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,13 @@
 "use strict";
 
+const body = document.querySelector("body");
 const mainBody = document.querySelector(".js-main");
 const overlayContainer = document.querySelector(".js-goldbeck-overlay");
-const firstLoad = localStorage.getItem("first_time");
 
-if (firstLoad) {
+if (!sessionStorage.visited) {
   window.addEventListener("load", function () {
+    overlayContainer.classList.remove("goldbeck-overlay__is-hidden");
+    body.classList.remove("goldbeck-overlay__background");
     setTimeout(() => {
       overlayContainer.classList.add("goldbeck-overlay__fade");
       mainBody.classList.remove("goldbeck-overlay__is-hidden");
@@ -14,7 +16,10 @@ if (firstLoad) {
     setTimeout(() => {
       overlayContainer.classList.add("goldbeck-overlay__is-hidden");
     }, 3750);
+    sessionStorage.visited = true;
   });
 } else {
+  body.classList.remove("goldbeck-overlay__background");
   overlayContainer.classList.add("goldbeck-overlay__is-hidden");
+  mainBody.classList.remove("goldbeck-overlay__is-hidden");
 }
