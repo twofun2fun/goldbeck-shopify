bef27552331a86b1b8eed7752b9abfb78556bc31
twofun2fun <tufan.calisir@outlook.de>
2022-06-19 20:06:32 +0200

    ----

--- assets/custom.js
+++ assets/custom.js
@@ -2,14 +2,19 @@
 
 const mainBody = document.querySelector(".js-main");
 const overlayContainer = document.querySelector(".js-goldbeck-overlay");
+const firstLoad = localStorage.getItem("first_time");
 
-window.addEventListener("load", function () {
-  setTimeout(() => {
-    overlayContainer.classList.add("goldbeck-overlay__fade");
-    mainBody.classList.remove("goldbeck-overlay__is-hidden");
-  }, 2000);
+if (firstLoad) {
+  window.addEventListener("load", function () {
+    setTimeout(() => {
+      overlayContainer.classList.add("goldbeck-overlay__fade");
+      mainBody.classList.remove("goldbeck-overlay__is-hidden");
+    }, 2000);
 
-  setTimeout(() => {
-    overlayContainer.classList.add("goldbeck-overlay__is-hidden");
-  }, 3750);
-});
+    setTimeout(() => {
+      overlayContainer.classList.add("goldbeck-overlay__is-hidden");
+    }, 3750);
+  });
+} else {
+  overlayContainer.classList.add("goldbeck-overlay__is-hidden");
+}
