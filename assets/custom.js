478bd7b46e791222b0d22a2a5f520802931a5403
twofun2fun <tufan.calisir@outlook.de>
2022-06-19 19:55:28 +0200

    ----

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,15 @@
 "use strict";
 
 const mainBody = document.querySelector(".js-main");
+const overlayContainer = document.querySelector(".js-goldbeck-overlay");
+
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    overlayContainer.classList.add("goldbeck-overlay__fade");
+    mainBody.classList.remove("goldbeck-overlay__is-hidden");
+  }, 2000);
+
+  setTimeout(() => {
+    overlayContainer.classList.add("goldbeck-overlay__is-hidden");
+  }, 3750);
+});
